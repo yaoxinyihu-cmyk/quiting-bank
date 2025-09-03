023c85e60a6c798821caab0354013898f3292160
HimHer45 <renjinyue1065@163.com>
2025-09-03 11:44:55 +0800

    闪图界面整合
    闪图界面整合完成，王果同学正在完善介绍页的AI部分

--- 待整合/闪图+介绍页+指南（正在完善AI）+整合好的“我的”/sum/pages/splash/splash.js
+++ 待整合/闪图+介绍页+指南（正在完善AI）+整合好的“我的”/sum/pages/splash/splash.js
@@ -2,7 +2,7 @@ Page({
     onLoad() {
       setTimeout(() => {
         wx.redirectTo({
-          url: '/pages/intro/intro' // 跳转到介绍页
+          url: '/pages/introduce/introduce' // 跳转到介绍页
         });
       }, 1000);
     }
